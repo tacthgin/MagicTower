9ecab2518069827488f6d9ed523e116d6a295cc9
陈起慧 <821910196@qq.com>
2022-01-04 16:28:17 +0800

    优化设置模型

---
+++ MagicTower/assets/Scripts/Model/SettingModel/SettingEvent.ts
@@ -0,0 +1,4 @@
+export enum SettingEvent {
+    MUSIC_ENABLED,
+    EFFECT_ENABLED,
+}
--- MagicTower/assets/Scripts/Model/SettingModel/SettingModel.ts
+++ MagicTower/assets/Scripts/Model/SettingModel/SettingModel.ts
@@ -1,6 +1,8 @@
-import { GameApp } from "../../GameFramework/Scripts/Application/GameApp";
-import { ModelBase } from "../../GameFramework/Scripts/Application/Model/ModelBase";
-import { ModelContainer } from "../../GameFramework/Scripts/Application/Model/ModelContainer";
+import { GameApp } from "../../../GameFramework/Scripts/Application/GameApp";
+import { ModelBase } from "../../../GameFramework/Scripts/Application/Model/ModelBase";
+import { ModelContainer } from "../../../GameFramework/Scripts/Application/Model/ModelContainer";
+import { SettingEvent } from "./SettingEvent";
+import { SettingEventArgs } from "./SettingEventArgs";
 
 @ModelContainer.registerModel("SettingModel")
 export class SettingModel extends ModelBase {
@@ -17,11 +19,23 @@ export class SettingModel extends ModelBase {
         if (this.musicEnabled === enabled) return;
         this.musicEnabled = enabled;
         GameApp.SoundManager.pauseSound(GameApp.SoundManager.backgroundSerialId);
+        this.save();
+        this.fireNow(SettingEventArgs.create(SettingEvent.MUSIC_ENABLED));
+    }
+
+    getMusicEnabled() {
+        return this.musicEnabled;
     }
 
     setEffectEnabled(enabled: boolean) {
         if (this.effectEnabled == enabled) return;
         this.effectEnabled = enabled;
         GameApp.SoundManager.stopAllSoundsExceptBackground();
+        this.save();
+        this.fireNow(SettingEventArgs.create(SettingEvent.EFFECT_ENABLED));
+    }
+
+    getEffectEnabled() {
+        return this.effectEnabled;
     }
 }
