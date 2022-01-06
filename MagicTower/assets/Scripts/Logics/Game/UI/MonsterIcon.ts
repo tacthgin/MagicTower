bd3cba4c05a405a4fd20ac51c3498aa2f074893a
陈起慧 <821910196@qq.com>
2022-01-06 17:07:42 +0800

    优化

--- MagicTower/assets/Scripts/Logics/Game/UI/MonsterIcon.ts
+++ MagicTower/assets/Scripts/Logics/Game/UI/MonsterIcon.ts
@@ -1,11 +1,11 @@
 import { Animation, Component, Sprite, _decorator } from "cc";
 import { Utility } from "../../../../GameFramework/Scripts/Utility/Utility";
-import { ElementManager } from "../Map/ElementManager";
+import { ElementFactory } from "../Map/ElementFactory";
 
 const { ccclass } = _decorator;
 
 @ccclass("MonsterIcon")
-export class MonsterIcon extends Component /*BasePoolNode*/ {
+export class MonsterIcon extends Component {
     private animation: Animation | null = null;
     private monsterInfo: any = null;
 
@@ -15,14 +15,14 @@ export class MonsterIcon extends Component /*BasePoolNode*/ {
 
     init(id: number) {
         this.monsterInfo = Utility.Json.getJsonElement("monster", id, true);
-        this.getComponent(Sprite)!.spriteFrame = ElementManager.getElementSpriteFrame(`${this.monsterInfo.spriteId}_0`);
+        this.getComponent(Sprite)!.spriteFrame = ElementFactory.getElementSpriteFrame(`${this.monsterInfo.spriteId}_0`);
         this.animation?.play(this.monsterInfo.spriteId);
     }
 
     createAnimationClip() {
         let spriteFrames = [];
         for (let i = 0; i < 2; i++) {
-            spriteFrames.push(ElementManager.getElementSpriteFrame(`${this.monsterInfo.spriteId}_${i}`));
+            spriteFrames.push(ElementFactory.getElementSpriteFrame(`${this.monsterInfo.spriteId}_${i}`));
         }
         return spriteFrames;
     }
