d4d469f8e1b97549ef2d478de09a372240af614d
陈起慧 <821910196@qq.com>
2022-01-07 17:09:24 +0800

    优化

--- MagicTower/assets/GameFramework/Scripts/NodePool/NodeObject.ts
+++ MagicTower/assets/GameFramework/Scripts/NodePool/NodeObject.ts
@@ -14,6 +14,7 @@ export class NodeObject extends ObjectBase {
 
     clear(): void {
         super.clear();
+        this._nodeHelper?.releaseNode(this.target);
         this._nodeHelper = null;
     }
 }
