652f6ef39e17663d6cc026869a1f15ae8a4948ce
陈起慧 <821910196@qq.com>
2022-02-09 16:15:04 +0800

    命令统一执行函数

--- MagicTower/assets/GameFramework/Scripts/Application/Command/CommandBase.ts
+++ MagicTower/assets/GameFramework/Scripts/Application/Command/CommandBase.ts
@@ -1,5 +1,6 @@
 import { IRerference } from "../../Base/ReferencePool/IRerference";
 
 export abstract class CommandBase implements IRerference {
+    abstract execute(): void;
     abstract clear(): void;
 }
