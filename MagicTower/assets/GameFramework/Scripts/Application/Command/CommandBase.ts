ac942ed72565078de316d63b3e8439012f32843f
陈起慧 <821910196@qq.com>
2022-02-10 11:03:19 +0800

    优化命令基类的执行函数

--- MagicTower/assets/GameFramework/Scripts/Application/Command/CommandBase.ts
+++ MagicTower/assets/GameFramework/Scripts/Application/Command/CommandBase.ts
@@ -1,6 +1,6 @@
 import { IRerference } from "../../Base/ReferencePool/IRerference";
 
 export abstract class CommandBase implements IRerference {
-    abstract execute(): void;
+    abstract execute(...args: any[]): void;
     abstract clear(): void;
 }
