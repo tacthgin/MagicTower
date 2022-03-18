585d5e0bd01e3ec3376b48843427ca867c9a4a5f
陈起慧 <821910196@qq.com>
2022-03-18 16:34:24 +0800

    去掉永驻节点逻辑，默认是在最上层的

--- MagicTower/assets/GameFramework/Scripts/Application/Scene/CSceneHelp.ts
+++ MagicTower/assets/GameFramework/Scripts/Application/Scene/CSceneHelp.ts
@@ -1,35 +1,8 @@
-import { director, Node, Scene } from "cc";
+import { director, Scene } from "cc";
 import { GameFrameworkLog } from "../../Base/Log/GameFrameworkLog";
 import { ISceneHelp } from "../../Scene/ISceneHelp";
 
 export class CSceneHelp implements ISceneHelp {
-    /**
-     * 永驻节点，切换场景的时候提高层级
-     */
-    private globalNode: Node | null = null;
-
-    /**
-     * 设置永驻节点
-     * @param node 永驻节点
-     */
-    setGlobalNode(node: Node) {
-        this.globalNode = node;
-    }
-
-    /**
-     * 调整永驻节点的层级
-     */
-    adjustGlobalNodeIndex() {
-        if (this.globalNode) {
-            let parent = this.globalNode.parent;
-            if (parent) {
-                this.globalNode.setSiblingIndex(parent.children.length);
-            }
-        } else {
-            GameFrameworkLog.warn("scene help global node not set");
-        }
-    }
-
     getScene<T extends object>(): T | null {
         return director.getScene() as T;
     }
@@ -41,7 +14,6 @@ export class CSceneHelp implements ISceneHelp {
                 if (error) {
                     GameFrameworkLog.error(error);
                 }
-                //this.adjustGlobalNodeIndex();
                 onLaunchedCallback && onLaunchedCallback();
             },
             () => {
