55b3c2beec55f4051b0fcce5db631ecad37f9083
陈起慧 <821910196@qq.com>
2022-03-29 15:17:37 +0800

    Auto stash before merge of "main" and "origin/main"

--- MagicTower/assets/GameFramework/Scripts/Application/Model/ModelBase.ts
+++ MagicTower/assets/GameFramework/Scripts/Application/Model/ModelBase.ts
@@ -1,16 +1,20 @@
 import { EventHandle } from "../../Base/EventPool/EventHandle";
 import { EventPool } from "../../Base/EventPool/EventPool";
 import { GameFrameworkError } from "../../Base/GameFrameworkError";
+import { GameFrameworkLog } from "../../Base/Log/GameFrameworkLog";
 import { ISaveManager } from "../../Save/ISaveManager";
 import { ScheduleBase } from "../Base/ScheduleBase";
 import { IModel } from "./IModel";
 import { ModelEventArgs } from "./ModelEventArgs";
 
+/**
+ * 模型基类
+ */
 export abstract class ModelBase extends ScheduleBase implements IModel {
+    private readonly _eventPool: EventPool<ModelEventArgs> = null!;
     private _saveManager: ISaveManager | null = null;
-    private _saveName: string = "";
     private _saveObject: any = {};
-    private _eventPool: EventPool<ModelEventArgs> = null!;
+    private _saveName: string = "";
 
     constructor() {
         super();
@@ -44,6 +48,7 @@ export abstract class ModelBase extends ScheduleBase implements IModel {
     /**
      * 设置存储管理器
      * @param saveManager 存储管理器
+     * @param saveName 模型存储名字
      */
     setSaveManager(saveManager: ISaveManager, saveName: string): void {
         this._saveManager = saveManager;
@@ -88,7 +93,9 @@ export abstract class ModelBase extends ScheduleBase implements IModel {
             throw new GameFrameworkError("you must set save manager first");
         }
         if (this._saveObject) {
-            this._saveManager.setObject(this._saveName, this);
+            this._saveManager.setObject(this._saveName, this._saveObject);
+        } else {
+            GameFrameworkLog.error(`you must set model ${this._saveName} save object first`);
         }
     }
 
@@ -109,5 +116,13 @@ export abstract class ModelBase extends ScheduleBase implements IModel {
                 thisInfo[key] = (data as any)[key];
             }
         }
+    }
+
+    /**
+     * 保存标志装饰器
+     * @returns 装饰器函数
+     */
+    static saveMark(...args: any[]) {
+        console.log(args);
     }
 }
