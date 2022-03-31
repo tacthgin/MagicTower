23ae6bd1c3c8f68a9e6ffdd82f78c99648b0c466
tacthgin <821910196@qq.com>
2022-03-31 08:39:41 +0800

    无用

--- MagicTower/assets/Scripts/Logics/Login/LoginScene.ts
+++ MagicTower/assets/Scripts/Logics/Login/LoginScene.ts
@@ -1,7 +1,6 @@
 import { AudioClip, Component, JsonAsset, Label, Prefab, SpriteFrame, TiledMapAsset, _decorator } from "cc";
 import { GameApp } from "../../../GameFramework/Scripts/Application/GameApp";
 import { Utility } from "../../../GameFramework/Scripts/Utility/Utility";
-import { SettingModel } from "../../Model/SettingModel/SettingModel";
 
 const { ccclass, property } = _decorator;
 
